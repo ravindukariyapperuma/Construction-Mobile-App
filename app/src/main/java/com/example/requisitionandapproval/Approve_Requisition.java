98b42e2e8dda97701d0c80c2655606eb6f472417
VishakaSubasith <subasith@gmail.com>
2020-09-23 10:10:53 +0530

    purchaseorder and approve requisition done

--- app/src/main/java/com/example/requisitionandapproval/Approve_Requisition.java
+++ app/src/main/java/com/example/requisitionandapproval/Approve_Requisition.java
@@ -1,14 +1,51 @@
 package com.example.requisitionandapproval;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Approve_Requisition extends AppCompatActivity {
+
+    List<ApproveModel> approveModels;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_approve__requisition);
+
+         recyclerView = findViewById(R.id.approveRV);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+//        ApproveModel[] approveModels = new ApproveModel[]{
+//
+//                new ApproveModel("Tokyo cement"),
+//                new ApproveModel("Pipes")
+//
+//
+//
+//        };
+//        ApproveAdapter adapter= new ApproveAdapter(approveModels,Approve_Requisition.this);
+//        recyclerView.setAdapter(adapter);
+        initdata();
+        initRecyclerView();
+    }
+    private void initRecyclerView() {
+        ApproveAdapter approveAdapter = new ApproveAdapter(approveModels);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(approveAdapter);
+    }
+    private void initdata(){
+        approveModels = new ArrayList<>();
+        approveModels.add(new ApproveModel("Tokya Cementt"));
+        approveModels.add(new ApproveModel("Pipes"));
+        approveModels.add(new ApproveModel("Brikes"));
+
     }
 }
---
+++ app/src/main/java/com/example/requisitionandapproval/place_purchase_Order_List.java
@@ -0,0 +1,14 @@
+package com.example.requisitionandapproval;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class place_purchase_Order_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_purchase__order__list);
+    }
+}
