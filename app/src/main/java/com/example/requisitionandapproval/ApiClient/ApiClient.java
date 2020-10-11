888cd872f80e663775737f1a7484627f1b68618e
Anuj Shashimal <anujshashimal456@gmail.com>
2020-10-11 21:34:24 +0530

    host updated

--- app/src/main/java/com/example/requisitionandapproval/ApiClient/ApiClient.java
+++ app/src/main/java/com/example/requisitionandapproval/ApiClient/ApiClient.java
@@ -1,7 +1,7 @@
 package com.example.requisitionandapproval.ApiClient;
 
 public class ApiClient {
-    private String BASE_URL = "http://10.0.2.2:3000";
+    private String BASE_URL = "http://constructionwebapp.alwaysdata.net";
 
     public String getBASE_URL() {
         return BASE_URL;
