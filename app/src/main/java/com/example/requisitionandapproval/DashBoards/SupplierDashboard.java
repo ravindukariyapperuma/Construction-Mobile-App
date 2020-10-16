76c7d0df14aacfeb0cdb10c6561d41849fbf1294
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-10-16 23:58:16 +0530

    dashboard-icons

--- app/src/main/java/com/example/requisitionandapproval/DashBoards/SupplierDashboard.java
+++ app/src/main/java/com/example/requisitionandapproval/DashBoards/SupplierDashboard.java
@@ -1,12 +1,16 @@
 package com.example.requisitionandapproval.DashBoards;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatRadioButton;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.requisitionandapproval.MainClasses.Suppliers.SupplierProfile;
@@ -16,9 +20,25 @@ public class SupplierDashboard extends AppCompatActivity {
 
     String EXTRA_SESSION_ID;
     ImageView supplierr;
+    AppCompatRadioButton rbAvailable, rbNotAvailable;
+    RadioButton rdActive, rdNotActive;
+    TextView txtUsername;
+//    String username ="abc";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        rbAvailable = findViewById(R.id.rbAvailable);
+        rbNotAvailable = findViewById(R.id.rbNotAvailable);
+
+//        rdActive = findViewById(R.id.rdActive);
+//        rdNotActive = findViewById(R.id.rdNotActive);
+//        rdActive.setChecked(true);
+
+
+        txtUsername = findViewById(R.id.txtUsername);
+//        txtUsername.setText(username);
+
         setContentView(R.layout.activity_supplier_dashboard);
 
         supplierr = findViewById(R.id.supplierr);
@@ -34,7 +54,36 @@ public class SupplierDashboard extends AppCompatActivity {
                 startActivity(intent1);
             }
         });
+
+
     }
+
+    public void onRadioButtonClick(View view){
+        boolean isSelected = ((AppCompatRadioButton)view).isChecked();
+//        switch (view.getId()){
+//            case R.id.rbAvailable:
+//                if(isSelected){
+//                    rbAvailable.setTextColor(Color.WHITE);
+//                    rbNotAvailable.setTextColor(Color.RED);
+//                }
+//                break;
+//            case R.id.rbNotAvailable:
+//                if(isSelected){
+//                    rbNotAvailable.setTextColor(Color.WHITE);
+//                    rbAvailable.setTextColor(Color.RED);
+//                }
+//                break;
+//        }
+//        if(rbAvailable.isChecked()){
+//            rbAvailable.setTextColor(Color.WHITE);
+//            rbNotAvailable.setTextColor(Color.RED);
+//        }else{
+//            rbNotAvailable.setTextColor(Color.WHITE);
+//            rbAvailable.setTextColor(Color.RED);
+//        }
+    }
+
+
     boolean doubleBackToExitPressedOnce = false;
 
     @Override
